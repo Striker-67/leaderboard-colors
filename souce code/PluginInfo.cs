853790f34fe42a5f1e7ce39ce117ad6b5cfee930
striker67 <86896895+FURY909@users.noreply.github.com>
2023-06-27 02:39:20 -0500

    Add files via upload

---
+++ souce code/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Mod3
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.author.gorillatag.mod3";
+        public const string Name = "Mod3";
+        public const string Version = "1.0.0";
+    }
+}
