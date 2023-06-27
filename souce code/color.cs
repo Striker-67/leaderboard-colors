375a7f8ed429cd59682b6929c776d8d64a752fab
striker67 <86896895+FURY909@users.noreply.github.com>
2023-06-27 01:46:31 -0500

    Add files via upload

---
+++ souce code/color.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace mainpart
+{
+    internal class color : MonoBehaviour
+    {
+        public Material newmat;
+        public GameObject board;
+        public void Start()
+        {
+            //Find the Standard Shader
+            newmat = new Material(Shader.Find("Standard"));
+
+            newmat.color = GameObject.Find("Global/Local VRRig/Local Gorilla Player/gorilla").GetComponent<SkinnedMeshRenderer>().material.color;
+
+            board = GameObject.Find("Level/forest/ForestObjects/campgroundstructure/scoreboard/REMOVE board");
+            board.GetComponent<Renderer>().material = newmat;
+            Update();
+        }
+        public void Update()
+        {
+            newmat.color = GameObject.Find("Global/Local VRRig/Local Gorilla Player/gorilla").GetComponent<SkinnedMeshRenderer>().material.color;
+        }
+    }
+}
