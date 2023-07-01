c89ec8ecbb6713e9345e3bf5466a94458ff1f6e4
striker67 <86896895+FURY909@users.noreply.github.com>
2023-06-30 21:32:10 -0500

    Add files via upload

--- source code/color.cs
+++ source code/color.cs
@@ -2,38 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-
+using GorillaNetworking;
 
 namespace mainpart
 {
     internal class color : MonoBehaviour
     {
         public Material newmat1;
-        public Material newMaterial;
-        public Material newMaterial2;
-        public int materialIndex ;
-        public int materialIndex2;
         public GameObject gorilla;
-        
+        public GameObject computer;
 
         public void Start()
         {
             //Find the Standard Shader
             newmat1 = new Material(Shader.Find("Standard"));
-
+            computer = GameObject.Find("Global/Photon Manager/GorillaComputer");
             
-            gorilla = GameObject.Find("Global/Local VRRig/Local Gorilla Player/gorilla");
+            gorilla = GameObject.Find("Global/Local VRRig/Local Gorilla Player");
 
             this.gameObject.GetComponent<MeshRenderer>().material = newmat1;
 
-
+            
 
         }
 
         public void Update()
         {
-            
-            this.gameObject.GetComponent<MeshRenderer>().material.color = gorilla.GetComponent<SkinnedMeshRenderer>().material.color; 
+
+            this.gameObject.GetComponent<MeshRenderer>().material.color = gorilla.GetComponent<VRRig>().playerColor;
 
 
 
