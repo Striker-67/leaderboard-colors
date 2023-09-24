f65d604d1d4929ddb904b8acadc68defb8e772f5
striker67 <86896895+FURY909@users.noreply.github.com>
2023-09-24 13:18:49 -0500

    Add files via upload

---
+++ soucre code/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaderBaordColors
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class info
+    {
+        public const string GUID = "com.striker67.gorillatag.leaderbaordcolors";
+        public const string Name = "LeaderBaordColors";
+        public const string Version = "1.0.0";
+    }
+}
---
+++ soucre code/color.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using GorillaNetworking;
+
+namespace mainpart
+{
+    internal class color : MonoBehaviour
+    {
+        public Material newmat1;
+        public GameObject gorilla;
+        public GameObject computer;
+
+        public void Start()
+        {
+            //Find the Standard Shader
+            newmat1 = new Material(Shader.Find("GorillaTag/UberShader"));
+            
+            gorilla = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player");
+
+            this.gameObject.GetComponent<MeshRenderer>().material = newmat1;
+
+            
+
+        }
+
+        public void Update()
+        {
+
+            this.gameObject.GetComponent<MeshRenderer>().material.color = gorilla.GetComponent<VRRig>().playerColor;
+
+
+
+        }
+    }
+}
