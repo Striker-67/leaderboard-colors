261abee229dbfc914f91c14d265e17d8b9845167
Cody <rodericgickle@gmail.com>
2025-06-23 12:58:37 -0700

    berer
    better

---
+++ LeaderBaordColors/LeaderBaordColors/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using LeaderBaordColors.Behaviors;
+
+namespace LeaderBaordColors
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Plugin()
+        {
+            gameObject.AddComponent<BoardManager>();
+            gameObject.AddComponent<ColorManager>();
+
+            ConfigManager.CreateConfig();
+        }
+    }
+}
